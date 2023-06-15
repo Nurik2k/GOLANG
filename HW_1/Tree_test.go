0d76fa7835d5e9c21f7c3a905c3d5dd05c811ccd
Nurik2k <nurkworld534@gmail.com>
2023-06-15 14:17:11 +0600

    Test fail

--- HW_1/Tree_test.go
+++ HW_1/Tree_test.go
@@ -16,11 +16,44 @@ func TestDirTree(t *testing.T) {
 	}
 
 	// Ожидаемый вывод
-	expectedOutput := `
-├───Tree.go (1797b)
-├───Tree_test.go (759b)
-└───go.mod (23b)
-`
+	expectedOutput := `├───Tree.go (1797b)
+        ├───Tree_test.go (2474b)
+        ├───go.mod (23b)
+        └───hw1_tree
+                ├───dockerfile (75b)
+                ├───hw1.md (4621b)
+                ├───main.go (352b)
+                ├───main_test.go (1865b)
+                └───testdata
+                        ├───project
+                        |       ├───file.txt (19b)
+                        |       └───gopher.png (70372b)
+                        ├───static
+                        |       ├───a_lorem
+                        |       |       ├───dolor.txt (0)
+                        |       |       ├───gopher.png (70372b)
+                        |       |       └───ipsum
+                        |       |               └───gopher.png (70372b)
+                        |       ├───css
+                        |       |       └───body.css (28b)
+                        |       ├───empty.txt (0)
+                        |       ├───html
+                        |       |       └───index.html (57b)
+                        |       ├───js
+                        |       |       └───site.js (10b)
+                        |       └───z_lorem
+                        |               ├───dolor.txt (0)
+                        |               ├───gopher.png (70372b)
+                        |               └───ipsum
+                        |                       └───gopher.png (70372b)
+                        ├───zline
+                        |       ├───empty.txt (0)
+                        |       └───lorem
+                        |               ├───dolor.txt (0)
+                        |               ├───gopher.png (70372b)
+                        |               └───ipsum
+                        |                       └───gopher.png (70372b)
+                        └───zzfile.txt (0)`
 
 	// Проверяем, что полученный вывод совпадает с ожидаемым выводом
 	if buf.String() != expectedOutput {
