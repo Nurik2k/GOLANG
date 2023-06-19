cec18916b3fd3cf457d4478d71341dd40cc44b09
Nurik2k <nurkworld534@gmail.com>
2023-06-19 14:51:49 +0600

    User struct

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module example.com/mymodule
 
 go 1.20
+
+require (
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+)
--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -4,6 +4,17 @@ import (
 	"io"
 )
 
+type User struct {
+	browsers []string `json:"browsers"`
+	company  string   `json:"company"`
+	country  string   `json:"country"`
+	email    string   `json:"email"`
+	job      string   `json:"job"`
+	name     string   `json:"name"`
+	phone    string   `json:"phone"`
+}
+
+// вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
 	SlowSearch(out)
 }
