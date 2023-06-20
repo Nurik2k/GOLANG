f03d59667c37d5e5e6f083b8c90b81ba47691ea7
Nurik2k <nurkworld534@gmail.com>
2023-06-20 14:23:15 +0600

    HW3 done

--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -1,11 +1,81 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
 	"io"
+	"os"
+	"regexp"
+	"strings"
 )
 
-// вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
+	r := regexp.MustCompile("@")
+	seenBrowsers := make(map[string]bool)
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	scanner := bufio.NewScanner(file)
+	lineCount := 0
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		lineCount++
+
+		user := make(map[string]interface{})
+		err := json.Unmarshal(line, &user)
+		if err != nil {
+			panic(err)
+		}
+
+		isAndroid := false
+		isMSIE := false
+
+		browsers, ok := user["browsers"].([]interface{})
+		if !ok {
+			continue
+		}
+
+		for _, browserRaw := range browsers {
+			browser, ok := browserRaw.(string)
+			if !ok {
+				continue
+			}
+
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+
+				if !seenBrowsers[browser] {
+					seenBrowsers[browser] = true
+					uniqueBrowsers++
+				}
+			}
+
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+
+				if !seenBrowsers[browser] {
+					seenBrowsers[browser] = true
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		email := r.ReplaceAllString(user["email"].(string), " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", lineCount-1, user["name"], email)
+	}
+
+	fmt.Fprintln(out, "found users:")
+	fmt.Fprintln(out, foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", uniqueBrowsers)
 }
