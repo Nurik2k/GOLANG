fe32afc057de8ab56481b994229257e2f983c4c7
Nurik2k <nurkworld534@gmail.com>
2023-06-23 16:40:24 +0600

    HW2 executePipeline -

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,3 +1,63 @@
 package main
 
-// сюда писать код
+import (
+	"fmt"
+	"sync"
+)
+
+func ExecutePipeline(jobs ...job) {
+	wg := &sync.WaitGroup{}
+
+	in := make(chan interface{})
+	out := make(chan interface{})
+
+	for _, i := range jobs {
+		wg.Add(1)
+		go func(in, out chan interface{}) {
+			defer wg.Done()
+
+		}(in, out)
+	}
+	wg.Wait()
+}
+
+func SingleHash(in, out chan interface{}) {
+
+}
+
+func MultiHash(in, out chan interface{}) {
+
+}
+
+func CombineResults(in, out chan interface{}) {
+
+}
+
+func main() {
+	jobs := []job{
+		job(func(in, out chan interface{}) {
+			out <- 1
+			//out <- 2
+			//out <- 3
+		}),
+		//job(func(in, out chan interface{}) {
+		//	for value := range in {
+		//		fmt.Println("This is JOB 2, sending data to out channel...")
+		//		out <- value
+		//	}
+		//}),
+		//job(func(in, out chan interface{}) {
+		//	for value := range in {
+		//		fmt.Println("This is JOB 3, sending data to out channel...")
+		//		out <- value
+		//	}
+		//}),
+		job(func(in, out chan interface{}) {
+			for value := range in {
+				fmt.Println("FINAL RESULT: ", value)
+			}
+		}),
+	}
+
+	ExecutePipeline(jobs...)
+}
