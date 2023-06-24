a561ca0ff1958469e96e692476dce8d0e5c62675
Nurik2k <nurkworld534@gmail.com>
2023-06-24 16:54:45 +0600

    HW2 Add go file- HashService

---
+++ hw2_signer/hashService.go
@@ -0,0 +1,11 @@
+package main
+
+import "sync"
+
+func SingleHashService(in, out chan interface{}, wg sync.WaitGroup, mutex sync.Mutex) {
+
+}
+
+func MultiHashService(in, out chan interface{}, wg sync.WaitGroup, mutex sync.Mutex) {
+
+}
