c4b97449e63b1b25c47510112b3eecd422660fbd
Nurik2k <nurkworld534@gmail.com>
2023-06-26 11:11:54 +0600

    SingleHash done

--- hw2_signer/hashService.go
+++ hw2_signer/hashService.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -8,13 +9,21 @@ func SingleHashService(in, out chan interface{}, wg sync.WaitGroup, mutex sync.M
 	var hash1 chan string
 	var hash2 chan string
 
+	defer close(hash2)
+	defer close(hash1)
+	defer wg.Done()
+
 	go func() {
 		hash1 <- DataSignerCrc32(ConvToString(in))
 	}()
-
+	mutex.Lock()
+	mutex.Unlock()
 	go func() {
 		hash2 <- DataSignerMd5(ConvToString(in))
 	}()
+
+	str := fmt.Sprintf("%s ~ %s", <-hash1, <-hash2)
+	out <- str
 }
 
 func MultiHashService(in, out chan interface{}, wg sync.WaitGroup, mutex sync.Mutex) {
