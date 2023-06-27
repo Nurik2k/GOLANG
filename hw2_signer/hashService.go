7b6702e9449a57b9b3c94c59f83c6d1e0a6e545f
Nurik2k <nurkworld534@gmail.com>
2023-06-27 18:53:01 +0600

    ExecutePipeline work, but test not work

--- hw2_signer/hashService.go
+++ hw2_signer/hashService.go
@@ -5,27 +5,42 @@ import (
 	"sync"
 )
 
-func SingleHashService(in interface{}, out chan interface{}, wg sync.WaitGroup, mutex sync.Mutex) {
-	var hash1 chan string
-	var hash2 chan string
-
-	defer close(hash2)
-	defer close(hash1)
-	defer wg.Done()
-
-	go func() {
-		hash1 <- DataSignerCrc32(ConvToString(in))
+func SingleHashService(in interface{}, out chan interface{}, wg *sync.WaitGroup, mutex *sync.Mutex) {
+	hash1 := make(chan string)
+	hash2 := make(chan string)
+	defer func() {
+		close(hash1)
+		close(hash2)
+		wg.Done()
 	}()
+
+	dataCrc32 := DataSignerCrc32(ConvToString(in))
+
+	go func(h1 chan string, d2 string) {
+		h1 <- d2
+	}(hash1, dataCrc32)
+
 	mutex.Lock()
+	dataMd5 := DataSignerMd5(ConvToString(in))
 	mutex.Unlock()
-	go func() {
-		hash2 <- DataSignerMd5(ConvToString(in))
-	}()
+
+	go func(h2 chan string, d2 string) {
+		h2 <- d2
+	}(hash2, dataMd5)
 
 	str := fmt.Sprintf("%s ~ %s", <-hash1, <-hash2)
 	out <- str
 }
 
-func MultiHashService(in, out chan interface{}, wg sync.WaitGroup, mutex sync.Mutex) {
+func MultiHashService(in interface{}, out chan interface{}, wg *sync.WaitGroup) {
+	hash := make(chan string)
 
+	defer close(hash)
+	defer wg.Done()
+
+	go func() {
+		hash <- DataSignerCrc32(ConvToString(in))
+	}()
+
+	out <- in
 }
--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -6,25 +6,22 @@ import (
 )
 
 func ExecutePipeline(jobs ...job) {
-	wg := &sync.WaitGroup{}
-
 	in := make(chan interface{})
 	out := make(chan interface{})
 
-	for _, i := range jobs {
-		wg.Add(1)
-		go func(job job, in, out chan interface{}) {
-			defer wg.Done()
+	wg := &sync.WaitGroup{}
 
-			i(in, out)
-		}(i, in, out)
+	for _, j := range jobs {
+
+		go j(in, out)
+		in = out
 	}
 	wg.Wait()
 }
 
 func SingleHash(in, out chan interface{}) {
-	wg := sync.WaitGroup{}
-	mutex := sync.Mutex{}
+	wg := &sync.WaitGroup{}
+	mutex := &sync.Mutex{}
 
 	for i := range in {
 		wg.Add(1)
@@ -35,40 +32,47 @@ func SingleHash(in, out chan interface{}) {
 }
 
 func MultiHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
 
-}
+	for i := range in {
 
-func CombineResults(in, out chan interface{}) {
-
+		go MultiHashService(i, out, wg)
+		wg.Done()
+	}
+	wg.Wait()
 }
 
 func main() {
+
 	jobs := []job{
 		job(func(in, out chan interface{}) {
 			out <- 1
-			//out <- 2
-			//out <- 3
+			out <- 2
+			out <- 3
 		}),
-		//job(func(in, out chan interface{}) {
-		//	for value := range in {
-		//		fmt.Println("This is JOB 2, sending data to out channel...")
-		//		out <- value
-		//	}
-		//}),
-		//job(func(in, out chan interface{}) {
-		//	for value := range in {
-		//		fmt.Println("This is JOB 3, sending data to out channel...")
-		//		out <- value
-		//	}
-		//}),
+		job(func(in, out chan interface{}) {
+			for value := range in {
+				fmt.Println("This is JOB 2, sending data to out channel...")
+				out <- value
+			}
+		}),
+		job(func(in, out chan interface{}) {
+			for value := range in {
+				fmt.Println("This is JOB 3, sending data to out channel...")
+				out <- value
+			}
+		}),
 		job(func(in, out chan interface{}) {
 			for value := range in {
 				fmt.Println("FINAL RESULT: ", value)
 			}
 		}),
 	}
-
 	ExecutePipeline(jobs...)
+}
+
+func CombineResults(in, out chan interface{}) {
+
 }
 
 func ConvToString(inter interface{}) string {
