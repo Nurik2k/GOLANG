7abc2be20ac22dd54ccb4cd3f5742229b3e8bb2e
Nurik2k <nurkworld534@gmail.com>
2023-06-29 17:12:46 +0600

    HW2 done

--- hw2_signer/hashService.go
+++ hw2_signer/hashService.go
@@ -1,46 +1,79 @@
 package main
 
 import (
-	"fmt"
+	"strings"
 	"sync"
 )
 
 func SingleHashService(in interface{}, out chan interface{}, wg *sync.WaitGroup, mutex *sync.Mutex) {
+
+	//Создаем срезы строк
 	hash1 := make(chan string)
 	hash2 := make(chan string)
+
+	//Функция, которая закрывает каналы и завершает работу горутины
 	defer func() {
 		close(hash1)
 		close(hash2)
 		wg.Done()
 	}()
 
-	dataCrc32 := DataSignerCrc32(ConvToString(in))
+	//Записываем в переменную dataCrc32, конвертированный интерфейс
+	dataCrc32 := ConvToString(in)
 
-	go func() {
-		hash1 <- dataCrc32
-	}()
+	//Горутина, которая записывает в hash1, данные из data шифрованные DataSignerCrc32
+	go func(data string, out chan string) {
+		out <- DataSignerCrc32(data)
+	}(dataCrc32, hash1)
 
-	mutex.Lock()
-	dataMd5 := DataSignerMd5(ConvToString(in))
-	mutex.Unlock()
+	mutex.Lock()                               //Блокируем для других горутин
+	dataMd5 := DataSignerMd5(ConvToString(in)) //Записываем в переменную dataMd5, конвертированный интерфейс и в то же время шифрование DataSignerMd5
+	mutex.Unlock()                             //Открываем для других горутин
 
-	go func() {
-		hash2 <- dataMd5
-	}()
+	//Горутина, которая записывает в hash2, данные из data шифрованные DataSignerCrc32
+	go func(data string, out chan string) {
+		out <- DataSignerCrc32(data)
+	}(dataMd5, hash2)
 
-	str := fmt.Sprintf("%s ~ %s", <-hash1, <-hash2)
-	out <- str
+	//Записываем в out результаты
+	out <- (<-hash1) + "~" + (<-hash2)
+
 }
 
 func MultiHashService(in interface{}, out chan interface{}, wg *sync.WaitGroup) {
-	hash := make(chan string)
 
-	defer close(hash)
-	defer wg.Done()
+	//Создаем срез канала строк и даем конкретный размер
+	hash := make([]chan string, 6)
+	//Данный цикл создает шесть каналов типа string и сохраняет их в слайс hash. Цикл выполняется для инициализации каналов и подготовки их к использованию.
+	for i := 0; i < 6; i++ {
+		hash[i] = make(chan string)
+	}
 
-	go func() {
-		hash <- DataSignerCrc32(ConvToString(in))
+	//Этот defer блок выполняет закрытие всех каналов в слайсе hash и вызов метода Done() для объекта wg типа sync.WaitGroup.
+	defer func() {
+		for _, ch := range hash {
+			close(ch)
+		}
+		wg.Done()
 	}()
 
-	out <- in
+	strBuilder := strings.Builder{} //Создаем новый объект типа strings.Builder{}
+	strItem := ConvToString(in)     //Новая переменная присваивает конвертированный интерфейс
+
+	//
+	for i, ch := range hash {
+		go func(data string, out chan string) {
+			out <- DataSignerCrc32(data)
+		}(ConvToString(i)+strItem, ch) // В данной строке кода используется функция ConvToString(i) для преобразования целочисленного значения i в строку. Затем к полученной строке добавляется значение переменной strItem.
+	}
+
+	//В данном цикле мы проходимся по элементам hash
+	for _, ch := range hash {
+		//Внутри цикла вызывается оператор <-ch, который блокирует выполнение горутины до тех пор, пока не будет получено значение из канала ch.
+		//Полученное значение строки с помощью оператора <- добавляется в strings.Builder с использованием метода WriteString().
+		strBuilder.WriteString(<-ch)
+	}
+
+	out <- strBuilder.String() //Записываем в out результаты
+
 }
--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -8,90 +8,63 @@ import (
 )
 
 func ExecutePipeline(jobs ...job) {
+	// Создаем канал для входных данных
 	in := make(chan interface{})
 	out := make(chan interface{})
-	wg := &sync.WaitGroup{}
+	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup для ожидания завершения всех горутин
 
-	for _, value := range jobs {
+	for _, value := range jobs { // Итерируемся по значениям в слайсе jobs
 		in = out
-		out = make(chan interface{}, MaxInputDataLen)
+		out = make(chan interface{}, MaxInputDataLen) // Создаем новый канал выходных данных для текущего job
 
 		wg.Add(1)
 
 		go func(job2 job, input, output chan interface{}) {
-			defer wg.Done()
-			defer close(output)
+			defer wg.Done()     // Уменьшаем счетчик WaitGroup на 1 при завершении горутины
+			defer close(output) // Закрываем канал output при завершении горутины
 
-			job2(input, output)
-		}(value, in, out)
+			job2(input, output) // Выполняем текущий job, передавая входной и выходной каналы
+		}(value, in, out) // Запускаем горутину для выполнения функции value с переданными аргументами value, in и out
 	}
 
-	wg.Wait()
+	wg.Wait() // Ожидаем завершения всех горутин, ожидающих в WaitGroup
 }
 
 func SingleHash(in, out chan interface{}) {
-	wg := &sync.WaitGroup{}
-	mutex := &sync.Mutex{}
+	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup для ожидания завершения всех горутин
+	mutex := &sync.Mutex{}  // Создаем объект sync.Mutex для синхронизации доступа к общим данным
 
-	for i := range in {
-		wg.Add(1)
-		go SingleHashService(i, out, wg, mutex)
+	for i := range in { // Итерируемся по значениям в канале in до его закрытия
+		wg.Add(1)                               // Увеличиваем счетчик WaitGroup на 1 перед запуском новой горутины
+		go SingleHashService(i, out, wg, mutex) // Запускаем горутину для выполнения SingleHashService с переданными аргументами i, out, wg и mutex
 	}
 
-	wg.Wait()
+	wg.Wait() // Ожидаем завершения всех горутин, ожидающих в WaitGroup
 }
 
 func MultiHash(in, out chan interface{}) {
-	wg := &sync.WaitGroup{}
+	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup для ожидания завершения всех горутин
 
-	for value := range in {
-		wg.Add(1)
-		go MultiHashService(value, out, wg)
+	for value := range in { // Итерируемся по значениям в канале in до его закрытия
+		wg.Add(1)                           // Увеличиваем счетчик WaitGroup на 1 перед запуском новой горутины
+		go MultiHashService(value, out, wg) // Запускаем горутину для выполнения MultiHashService с переданными аргументами value, out и wg
 	}
-	wg.Wait()
-}
 
-func main() {
-
-	jobs := []job{
-		job(func(in, out chan interface{}) {
-			out <- 1
-			out <- 2
-			out <- 3
-		}),
-		job(func(in, out chan interface{}) {
-			for value := range in {
-				fmt.Println("This is JOB 2, sending data to out channel...")
-				out <- value
-			}
-		}),
-		job(func(in, out chan interface{}) {
-			for value := range in {
-				fmt.Println("This is JOB 3, sending data to out channel...")
-				out <- value
-			}
-		}),
-		job(func(in, out chan interface{}) {
-			for value := range in {
-				fmt.Println("FINAL RESULT: ", value)
-			}
-		}),
-	}
-	ExecutePipeline(jobs...)
+	wg.Wait() // Ожидаем завершения всех горутин, ожидающих в WaitGroup
 }
 
 func CombineResults(in, out chan interface{}) {
-	inputValue := make([]string, MaxInputDataLen)
+	inputValue := make([]string, 0, MaxInputDataLen) // Создаем пустой слайс типа string с начальной емкостью MaxInputDataLen
 
-	for value := range in {
-		inputValue = append(inputValue, ConvToString(value))
+	for value := range in { // Итерируемся по значениям в канале in до его закрытия
+		inputValue = append(inputValue, ConvToString(value)) // Преобразуем значение в строку с помощью ConvToString и добавляем его в слайс inputValue
 	}
 
-	sort.SliceIsSorted(inputValue, func(i, j int) bool {
-		return inputValue[i] < inputValue[j]
+	sort.Slice(inputValue, func(i, j int) bool { // Сортируем слайс inputValue с использованием анонимной функции сравнения
+		return inputValue[i] < inputValue[j] // Возвращаем true, если элемент с индексом i меньше элемента с индексом j
 	})
 
-	out <- strings.Join(inputValue, "_")
+	out <- strings.Join(inputValue, "_") // Преобразуем слайс inputValue в строку, объединяя элементы с помощью разделителя "_" и отправляем результат в канал out
 }
 
 func ConvToString(inter interface{}) string {
