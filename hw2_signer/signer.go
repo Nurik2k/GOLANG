e173241cbba4673646434a69041203fc0690cd19
Nurik2k <nurkworld534@gmail.com>
2023-06-30 11:22:18 +0600

    HW4 start

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -11,7 +11,7 @@ func ExecutePipeline(jobs ...job) {
 	// Создаем канал для входных данных
 	in := make(chan interface{})
 	out := make(chan interface{})
-	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup для ожидания завершения всех горутин
+	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup{} для ожидания завершения всех горутин
 
 	for _, value := range jobs { // Итерируемся по значениям в слайсе jobs
 		in = out
@@ -31,8 +31,8 @@ func ExecutePipeline(jobs ...job) {
 }
 
 func SingleHash(in, out chan interface{}) {
-	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup для ожидания завершения всех горутин
-	mutex := &sync.Mutex{}  // Создаем объект sync.Mutex для синхронизации доступа к общим данным
+	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup{} для ожидания завершения всех горутин
+	mutex := &sync.Mutex{}  // Создаем объект sync.Mutex{} для синхронизации доступа к общим данным
 
 	for i := range in { // Итерируемся по значениям в канале in до его закрытия
 		wg.Add(1)                               // Увеличиваем счетчик WaitGroup на 1 перед запуском новой горутины
@@ -43,7 +43,7 @@ func SingleHash(in, out chan interface{}) {
 }
 
 func MultiHash(in, out chan interface{}) {
-	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup для ожидания завершения всех горутин
+	wg := &sync.WaitGroup{} // Создаем объект sync.WaitGroup{} для ожидания завершения всех горутин
 
 	for value := range in { // Итерируемся по значениям в канале in до его закрытия
 		wg.Add(1)                           // Увеличиваем счетчик WaitGroup на 1 перед запуском новой горутины
@@ -54,10 +54,10 @@ func MultiHash(in, out chan interface{}) {
 }
 
 func CombineResults(in, out chan interface{}) {
-	inputValue := make([]string, 0, MaxInputDataLen) // Создаем пустой слайс типа string с начальной емкостью MaxInputDataLen
+	inputValue := make([]string, 0, MaxInputDataLen) // Создаем пустой слайс типа string с начальной емкостью MaxInputDataLen = 100
 
 	for value := range in { // Итерируемся по значениям в канале in до его закрытия
-		inputValue = append(inputValue, ConvToString(value)) // Преобразуем значение в строку с помощью ConvToString и добавляем его в слайс inputValue
+		inputValue = append(inputValue, ConvToString(value)) // Преобразуем значение в строку с помощью ConvToString() и добавляем его в слайс inputValue
 	}
 
 	sort.Slice(inputValue, func(i, j int) bool { // Сортируем слайс inputValue с использованием анонимной функции сравнения
@@ -67,6 +67,7 @@ func CombineResults(in, out chan interface{}) {
 	out <- strings.Join(inputValue, "_") // Преобразуем слайс inputValue в строку, объединяя элементы с помощью разделителя "_" и отправляем результат в канал out
 }
 
+// Функция конвертации интерфейса в строку
 func ConvToString(inter interface{}) string {
 	str := fmt.Sprintf("%v", inter)
 	return str
