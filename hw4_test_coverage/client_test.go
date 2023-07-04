d4bb3f285c1a6cef2a718ee2e600582204582b6f
Nurik2k <nurkworld534@gmail.com>
2023-07-04 15:29:29 +0600

    Readfile and Unmarshal

--- hw4_test_coverage/client_test.go
+++ hw4_test_coverage/client_test.go
@@ -1,8 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+)
 
-// Get запрос
+type xmlUser struct {
+	XmlName xml.Name `xml:"root"`
+	Row     []struct {
+		Id     int    `xml:"id"`
+		Name   string `xml:"first_name"`
+		Age    int    `xml:"age"`
+		About  string `xml:"about"`
+		Gender string `xml:"gender"`
+	} `xml:"row"`
+}
+
 func SearchServer(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("query")
+	orderField := r.URL.Query().Get("order_field")
+	var users User
 
+	xmlData, err := ioutil.ReadFile("dataset.xml")
+	if err != nil {
+		http.Error(w, "Данные не прочитаны", 500)
+		return
+	}
+
+	err = xml.Unmarshal(xmlData, users)
+	if err != nil {
+		http.Error(w, "Данные не переведены", 500)
+		return
+	}
 }
