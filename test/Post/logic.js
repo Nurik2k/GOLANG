9c1b445cbd1a0c004cbf452bd6591726d42b9607
Nurik2k <nurkworld534@gmail.com>
2023-07-26 00:05:20 +0600

    Post end

--- test/Post/logic.js
+++ test/Post/logic.js
@@ -1,4 +1,4 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
+document.getElementById("login-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
     const login = document.getElementById("login").value;
@@ -7,27 +7,25 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     // Создаем объект с данными для отправки на сервер
     const data = {
         login: login,
-      password: password
-    };
+        password: password
+      };
 
     const url = "http://localhost:8080/login";
 
     const options = {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      },
-      mode: "no-cors" // Set 'no-cors' mode to bypass CORS restrictions"
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        },
+        mode: "no-cors"
     };
 
-    // Отправляем POST-запрос на сервер Golang
+  // Отправляем POST-запрос на сервер Golang
     fetch(url, options)
     .then(response => response.json())
-    .then(data => {
-      alert(data.message); // Выводим сообщение от сервера
-    })
+    .then(data =>{window.location.href = "Hello.html"})
     .catch(error => {
       console.log("Ошибка:", error);
     });
-  });
+});
